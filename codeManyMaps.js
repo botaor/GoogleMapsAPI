105eda22fb43ba07e443de7e828337c7d3ac0372
Rui Botão <rui.botao@ruibotao.com>
2015-01-07 10:13:06 +0000

    ADD: Add bounds_changed event handler

--- codeManyMaps.js
+++ codeManyMaps.js
@@ -1,3 +1,24 @@
+function sleep(delay)
+{
+    var start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
+}
+      
+var map = null ;
+
+function boundsChange()
+{
+    document.getElementById("zoomlevel").value = map.getZoom() ;
+    
+    var ne = map.getBounds().getNorthEast() ;
+    var sw = map.getBounds().getSouthWest() ;
+    
+    document.getElementById( "result" ).value = "bounds: " + ne.lat() + ',' + ne.lng() + '\n' +
+                                                "        " + sw.lat() + ',' + sw.lng() + '\n';  
+    //simulate read from database
+    //sleep( 200 ) ;
+}    
+
 function initialize()
 {
     var homeLocation = new google.maps.LatLng(39.499903,-8.719593)
@@ -10,6 +31,8 @@ function initialize()
         streetViewControl: false
     };
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    
+    google.maps.event.addListener( map, 'bounds_changed', boundsChange );    
 }
 
 function loadGoogleMapsScript()
